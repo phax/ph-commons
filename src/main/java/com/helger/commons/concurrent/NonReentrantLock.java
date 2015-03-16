6f27d6917e31bb3dc5ae2f4efd07e270fa95e6d8
Philip Helger <philip@helger.com>
2015-03-16 19:49:44 +0100

    Added class NonReentrantLock from Netty

---
+++ src/main/java/com/helger/commons/concurrent/NonReentrantLock.java
@@ -0,0 +1,79 @@
+package com.helger.commons.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Non reentrant lock. Copied from Netty 3.7 sources.
+ *
+ * @author Philip Helger
+ * @since 5.6.1
+ */
+public final class NonReentrantLock extends AbstractQueuedSynchronizer implements Lock
+{
+  private Thread m_aOwner;
+
+  public void lock ()
+  {
+    acquire (1);
+  }
+
+  public void lockInterruptibly () throws InterruptedException
+  {
+    acquireInterruptibly (1);
+  }
+
+  public boolean tryLock ()
+  {
+    return tryAcquire (1);
+  }
+
+  public boolean tryLock (final long time, final TimeUnit unit) throws InterruptedException
+  {
+    return tryAcquireNanos (1, unit.toNanos (time));
+  }
+
+  public void unlock ()
+  {
+    release (1);
+  }
+
+  public boolean isHeldByCurrentThread ()
+  {
+    return isHeldExclusively ();
+  }
+
+  public Condition newCondition ()
+  {
+    return new ConditionObject ();
+  }
+
+  @Override
+  protected boolean tryAcquire (final int acquires)
+  {
+    if (compareAndSetState (0, 1))
+    {
+      m_aOwner = Thread.currentThread ();
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  protected boolean tryRelease (final int releases)
+  {
+    if (Thread.currentThread () != m_aOwner)
+      throw new IllegalMonitorStateException ();
+    m_aOwner = null;
+    setState (0);
+    return true;
+  }
+
+  @Override
+  protected boolean isHeldExclusively ()
+  {
+    return getState () != 0 && m_aOwner == Thread.currentThread ();
+  }
+}
