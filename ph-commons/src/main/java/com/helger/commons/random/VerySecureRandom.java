3c1f983ca8727abdd21aaa4c09886f77aa0d1a60
phax <philip@helger.com>
2015-08-13 09:31:46 +0200

    Added some debug logging

--- ph-commons/src/main/java/com/helger/commons/random/VerySecureRandom.java
+++ ph-commons/src/main/java/com/helger/commons/random/VerySecureRandom.java
@@ -21,6 +21,9 @@ import java.security.SecureRandom;
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.helger.commons.annotation.PresentForCodeCoverage;
 
 /**
@@ -31,30 +34,61 @@ import com.helger.commons.annotation.PresentForCodeCoverage;
 @Immutable
 public final class VerySecureRandom
 {
+  private static final Logger s_aLogger = LoggerFactory.getLogger (VerySecureRandom.class);
+
   private static final SecureRandom s_aSecureRandom;
 
-  private static SecureRandom _getSecureRandomInstance ()
+  /**
+   * Create a new {@link SecureRandom} instance. First the IBM secure random is
+   * tried, than the SHA1PRNG secure random and finally if the previous ones
+   * failed, the default instance is used. In certain circumstances (Linux +
+   * some Java version) this initialization takes forever and that's why the
+   * debug statements are added.
+   *
+   * @return A new {@link SecureRandom} instance. Never <code>null</code>.
+   */
+  @Nonnull
+  private static SecureRandom _createSecureRandomInstance ()
   {
     SecureRandom aSecureRandom;
     try
     {
+      if (s_aLogger.isDebugEnabled ())
+        s_aLogger.debug ("Trying to get SecureRandom: IBMSecureRandom, IBMJCE");
+
       // IBM JCE
       // http://www.ibm.com/developerworks/java/jdk/security/50/secguides/JceDocs/api_users_guide.html
       aSecureRandom = SecureRandom.getInstance ("IBMSecureRandom", "IBMJCE");
+
+      if (s_aLogger.isDebugEnabled ())
+        s_aLogger.debug ("Using SecureRandom: IBMSecureRandom, IBMJCE");
     }
     catch (final Throwable t)
     {
       try
       {
-        // SUN
+        if (s_aLogger.isDebugEnabled ())
+          s_aLogger.debug ("Trying to get SecureRandom: SHA1PRNG");
+
+        // Oracle
         // http://docs.oracle.com/javase/6/docs/technotes/guides/security/crypto/CryptoSpec.html
         // http://docs.oracle.com/javase/7/docs/technotes/guides/security/crypto/CryptoSpec.html
+        // http://docs.oracle.com/javase/8/docs/technotes/guides/security/crypto/CryptoSpec.html
         aSecureRandom = SecureRandom.getInstance ("SHA1PRNG");
+
+        if (s_aLogger.isDebugEnabled ())
+          s_aLogger.debug ("Using SecureRandom: SHA1PRNG");
       }
       catch (final Throwable t2)
       {
+        if (s_aLogger.isDebugEnabled ())
+          s_aLogger.debug ("Trying to get default SecureRandom");
+
         // Default
         aSecureRandom = new SecureRandom ();
+
+        if (s_aLogger.isDebugEnabled ())
+          s_aLogger.debug ("Using default SecureRandom");
       }
     }
     return aSecureRandom;
@@ -66,7 +100,7 @@ public final class VerySecureRandom
     // This is a lengthy operation, to be done only upon
     // initialization of the application. Especial with Java <= 1.5 this whole
     // block takes more or less forever.
-    final SecureRandom aSecureRandom = _getSecureRandomInstance ();
+    final SecureRandom aSecureRandom = _createSecureRandomInstance ();
 
     // Get 128 random bytes
     aSecureRandom.nextBytes (new byte [128]);
@@ -75,7 +109,7 @@ public final class VerySecureRandom
     final byte [] aSeed = aSecureRandom.generateSeed (10);
 
     // Initialize main secure random
-    s_aSecureRandom = _getSecureRandomInstance ();
+    s_aSecureRandom = _createSecureRandomInstance ();
     s_aSecureRandom.setSeed (aSeed);
   }
 
