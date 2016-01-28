bf21ca62b6b0c3bf778e2916d6403e27beeebd80
Philip Helger <philip@helger.com>
2016-01-28 10:39:29 +0100

    Extended API

--- ph-commons/src/main/java/com/helger/commons/ws/AbstractWSClientCaller.java
+++ ph-commons/src/main/java/com/helger/commons/ws/AbstractWSClientCaller.java
@@ -17,7 +17,9 @@
 package com.helger.commons.ws;
 
 import java.net.URL;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -27,13 +29,22 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSocketFactory;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.MessageContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.Nonempty;
 import com.helger.commons.annotation.OverrideOnDemand;
+import com.helger.commons.annotation.ReturnsMutableCopy;
+import com.helger.commons.collection.CollectionHelper;
+import com.helger.commons.collection.multimap.IMultiMapListBased;
+import com.helger.commons.collection.multimap.MultiLinkedHashMapArrayListBased;
 import com.helger.commons.lang.ClassLoaderHelper;
+import com.helger.commons.state.EChange;
+import com.helger.commons.state.ETriState;
+import com.helger.commons.string.StringHelper;
 
 /**
  * Abstract base class for a webservice client caller.
@@ -53,6 +64,11 @@ public abstract class AbstractWSClientCaller
   private HostnameVerifier m_aHostnameVerifier;
   private int m_nConnectionTimeoutMS = DEFAULT_CONNECTION_TIMEOUT_MS;
   private int m_nRequestTimeoutMS = DEFAULT_REQUEST_TIMEOUT_MS;
+  private String m_sUserName;
+  private String m_sPassword;
+  private String m_sSOAPAction;
+  private IMultiMapListBased <String, String> m_aHTTPHeaders;
+  private ETriState m_eCookiesSupport = ETriState.UNDEFINED;
 
   private boolean m_bWorkAroundMASM0003 = true;
 
@@ -127,7 +143,8 @@ public abstract class AbstractWSClientCaller
   }
 
   /**
-   * @return The connection timeout in milliseconds.
+   * @return The connection timeout in milliseconds. Default is
+   *         {@link #DEFAULT_CONNECTION_TIMEOUT_MS}.
    */
   public int getConnectionTimeoutMS ()
   {
@@ -146,7 +163,8 @@ public abstract class AbstractWSClientCaller
   }
 
   /**
-   * @return The request timeout in milliseconds.
+   * @return The request timeout in milliseconds. Default is
+   *         {@link #DEFAULT_REQUEST_TIMEOUT_MS}.
    */
   public int getRequestTimeoutMS ()
   {
@@ -163,6 +181,320 @@ public abstract class AbstractWSClientCaller
   {
     m_nRequestTimeoutMS = nRequestTimeoutMS;
   }
+
+  /**
+   * @return The user name for HTTP Basic Auth. May be <code>null</code>.
+   *         Default is <code>null</code>.
+   */
+  @Nullable
+  public String getUserName ()
+  {
+    return m_sUserName;
+  }
+
+  /**
+   * @return <code>true</code> if a HTTP Basic Auth user name is defined,
+   *         <code>false</code> otherwise.
+   */
+  public boolean hasUserName ()
+  {
+    return StringHelper.hasText (m_sUserName);
+  }
+
+  /**
+   * Set the HTTP Basic Auth user name to be used.
+   *
+   * @param sUserName
+   *        The user name to use. May be <code>null</code>.
+   */
+  public void setUserName (@Nullable final String sUserName)
+  {
+    m_sUserName = sUserName;
+  }
+
+  /**
+   * @return The password for HTTP Basic Auth. May be <code>null</code>. Default
+   *         is <code>null</code>.
+   */
+  @Nullable
+  public String getPassword ()
+  {
+    return m_sPassword;
+  }
+
+  /**
+   * @return <code>true</code> if a HTTP Basic Auth password is defined,
+   *         <code>false</code> otherwise.
+   */
+  public boolean hasPassword ()
+  {
+    return StringHelper.hasText (m_sPassword);
+  }
+
+  /**
+   * Set the HTTP Basic Auth password to be used.
+   *
+   * @param sPassword
+   *        The password to use. May be <code>null</code>.
+   */
+  public void setPassword (@Nullable final String sPassword)
+  {
+    m_sPassword = sPassword;
+  }
+
+  /**
+   * @return The SOAP Action to be used. May be <code>null</code>. Default is
+   *         <code>null</code>.
+   */
+  @Nullable
+  public String getSOAPAction ()
+  {
+    return m_sSOAPAction;
+  }
+
+  /**
+   * @return <code>true</code> if a SOAP Action is defined, <code>false</code>
+   *         otherwise.
+   */
+  public boolean hasSOAPAction ()
+  {
+    return StringHelper.hasText (m_sSOAPAction);
+  }
+
+  /**
+   * Set the SOAP Action to be used.
+   *
+   * @param sSOAPAction
+   *        The SOAP Action to use. May be <code>null</code>.
+   */
+  public void setSOAPAction (@Nullable final String sSOAPAction)
+  {
+    m_sSOAPAction = sSOAPAction;
+  }
+
+  @Nonnull
+  private IMultiMapListBased <String, String> _getHeaderMap ()
+  {
+    if (m_aHTTPHeaders == null)
+      m_aHTTPHeaders = new MultiLinkedHashMapArrayListBased <> ();
+    return m_aHTTPHeaders;
+  }
+
+  /**
+   * Add an HTTP header to be send. If another HTTP header with the same name is
+   * already present the passed value is appended!
+   *
+   * @param sName
+   *        Header name. May neither be <code>null</code> nor empty.
+   * @param sValue
+   *        Header value. May not be <code>null</code>.
+   * @see #addHTTPHeader(String, Collection)
+   * @see #setHTTPHeader(String, String)
+   * @see #setHTTPHeader(String, Collection)
+   */
+  public void addHTTPHeader (@Nonnull @Nonempty final String sName, @Nonnull final String sValue)
+  {
+    ValueEnforcer.notEmpty (sName, "Name");
+    ValueEnforcer.notNull (sValue, "Value");
+
+    _getHeaderMap ().putSingle (sName, sValue);
+  }
+
+  /**
+   * Add an HTTP header to be send. This method adds values to an existing
+   * header with the same name.
+   *
+   * @param sName
+   *        Header name. May neither be <code>null</code> nor empty.
+   * @param aValues
+   *        Header values to be added. May not be <code>null</code> but may not
+   *        contain <code>null</code> values. If this collection is empty
+   *        nothing happens.
+   * @see #addHTTPHeader(String, String)
+   * @see #setHTTPHeader(String, String)
+   * @see #setHTTPHeader(String, Collection)
+   */
+  public void addHTTPHeader (@Nonnull @Nonempty final String sName, @Nonnull final Collection <String> aValues)
+  {
+    ValueEnforcer.notEmpty (sName, "Name");
+    ValueEnforcer.notNullNoNullValue (aValues, "Values");
+
+    _getHeaderMap ().getOrCreate (sName).addAll (aValues);
+  }
+
+  /**
+   * Add an HTTP header to be send. If another HTTP header with the same name is
+   * already present it will be overwritten!
+   *
+   * @param sName
+   *        Header name. May neither be <code>null</code> nor empty.
+   * @param sValue
+   *        Header value. May not be <code>null</code>.
+   * @see #addHTTPHeader(String, String)
+   * @see #addHTTPHeader(String, Collection)
+   * @see #setHTTPHeader(String, Collection)
+   */
+  public void setHTTPHeader (@Nonnull @Nonempty final String sName, @Nonnull final String sValue)
+  {
+    ValueEnforcer.notEmpty (sName, "Name");
+    ValueEnforcer.notNull (sValue, "Value");
+
+    _getHeaderMap ().put (sName, CollectionHelper.newList (sValue));
+  }
+
+  /**
+   * Add an HTTP header to be send. This method overwrites an existing header
+   * with the same name.
+   *
+   * @param sName
+   *        Header name. May neither be <code>null</code> nor empty.
+   * @param aValues
+   *        Header values to be added. May not be <code>null</code> but may not
+   *        contain <code>null</code> values. If this collection is empty the
+   *        header will be removed.
+   * @see #addHTTPHeader(String, String)
+   * @see #addHTTPHeader(String, Collection)
+   * @see #setHTTPHeader(String, String)
+   */
+  public void setHTTPHeader (@Nonnull @Nonempty final String sName, @Nonnull final Collection <String> aValues)
+  {
+    ValueEnforcer.notEmpty (sName, "Name");
+    ValueEnforcer.notNullNoNullValue (aValues, "Values");
+
+    if (aValues.isEmpty ())
+      removeHTTPHeader (sName);
+    else
+      _getHeaderMap ().put (sName, CollectionHelper.newList (aValues));
+  }
+
+  /**
+   * Remove all HTTP header values with the passed name.
+   *
+   * @param sName
+   *        Header name. May be <code>null</code>.
+   * @return {@link EChange#CHANGED} if remove was successful.
+   */
+  @Nonnull
+  public EChange removeHTTPHeader (@Nonnull final String sName)
+  {
+    if (m_aHTTPHeaders == null || StringHelper.hasNoText (sName))
+      return EChange.UNCHANGED;
+
+    return EChange.valueOf (m_aHTTPHeaders.remove (sName) != null);
+  }
+
+  /**
+   * Remove the HTTP header with the passed name and value.
+   *
+   * @param sName
+   *        Header name. May be <code>null</code>.
+   * @param sValue
+   *        Header value. May be <code>null</code>.
+   * @return {@link EChange#CHANGED} if remove was successful.
+   */
+  @Nonnull
+  public EChange removeHTTPHeader (@Nullable final String sName, @Nullable final String sValue)
+  {
+    if (m_aHTTPHeaders == null || StringHelper.hasNoText (sName) || sValue == null)
+      return EChange.UNCHANGED;
+
+    final List <String> aValues = m_aHTTPHeaders.get (sName);
+    if (aValues != null)
+      if (aValues.remove (sValue))
+      {
+        // If no more value for the passed name is present, remove the whole
+        // list
+        if (aValues.isEmpty ())
+          m_aHTTPHeaders.remove (sName);
+
+        return EChange.CHANGED;
+      }
+    return EChange.UNCHANGED;
+  }
+
+  /**
+   * @param sName
+   *        HTTP header name
+   * @return <code>true</code> if the HTTP is contained and contains at least
+   *         one value.
+   */
+  public boolean containsHTTPHeader (@Nullable final String sName)
+  {
+    if (m_aHTTPHeaders == null || StringHelper.hasNoText (sName))
+      return false;
+    return CollectionHelper.isNotEmpty (m_aHTTPHeaders.get (sName));
+  }
+
+  /**
+   * @param sName
+   *        HTTP header name. May be <code>null</code>.
+   * @return The list of all HTTP header values registered for the past name.
+   *         Never <code>null</code> but maybe empty.
+   */
+  @Nonnull
+  @ReturnsMutableCopy
+  public List <String> getAllHTTPHeaderValues (@Nullable final String sName)
+  {
+    if (m_aHTTPHeaders == null || StringHelper.hasNoText (sName))
+      return CollectionHelper.newList (0);
+    return CollectionHelper.newList (m_aHTTPHeaders.get (sName));
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public Map <String, List <String>> getAllHTTPHeaders ()
+  {
+    return CollectionHelper.newOrderedMap (m_aHTTPHeaders);
+  }
+
+  /**
+   * Forces this client to send compressed HTTP content. Disabled by default.
+   *
+   * @param bCompress
+   *        <code>true</code> to enable, <code>false</code> to disable.
+   */
+  public void setCompressedRequest (final boolean bCompress)
+  {
+    if (bCompress)
+      setHTTPHeader ("Content-Encoding", "gzip");
+    else
+      removeHTTPHeader ("Content-Encoding");
+  }
+
+  /**
+   * Add a hint that this client understands compressed HTTP content. Disabled
+   * by default.
+   *
+   * @param bCompress
+   *        <code>true</code> to enable, <code>false</code> to disable.
+   */
+  public void setCompressedResponse (final boolean bCompress)
+  {
+    if (bCompress)
+      setHTTPHeader ("Accept-Encoding", "gzip");
+    else
+      removeHTTPHeader ("Accept-Encoding");
+  }
+
+  public boolean isCookiesSupportEnabled ()
+  {
+    return m_eCookiesSupport.isTrue ();
+  }
+
+  public void setCookiesSupportEnabled (final boolean bEnabled)
+  {
+    m_eCookiesSupport = ETriState.valueOf (bEnabled);
+  }
+
+  /**
+   * Add custom properties to the request context.
+   * 
+   * @param aRequestContext
+   *        The request context to be filled. Never <code>null</code>.
+   */
+  @OverrideOnDemand
+  protected void customizeRequestContext (@Nonnull final Map <String, Object> aRequestContext)
+  {}
 
   /**
    * Implement this method in your derived class to add custom handlers.
@@ -189,32 +521,49 @@ public abstract class AbstractWSClientCaller
   @OverridingMethodsMustInvokeSuper
   protected void applyWSSettingsToBindingProvider (@Nonnull final BindingProvider aBP)
   {
-    aBP.getRequestContext ().put (BindingProvider.ENDPOINT_ADDRESS_PROPERTY, m_aEndpointAddress.toString ());
+    final Map <String, Object> aRequestContext = aBP.getRequestContext ();
+
+    aRequestContext.put (BindingProvider.ENDPOINT_ADDRESS_PROPERTY, m_aEndpointAddress.toExternalForm ());
     if (m_aSSLSocketFactory != null)
     {
-      aBP.getRequestContext ().put ("com.sun.xml.ws.transport.https.client.SSLSocketFactory", m_aSSLSocketFactory);
-      aBP.getRequestContext ().put ("com.sun.xml.internal.ws.transport.https.client.SSLSocketFactory",
-                                    m_aSSLSocketFactory);
+      aRequestContext.put ("com.sun.xml.ws.transport.https.client.SSLSocketFactory", m_aSSLSocketFactory);
+      aRequestContext.put ("com.sun.xml.internal.ws.transport.https.client.SSLSocketFactory", m_aSSLSocketFactory);
     }
     if (m_aHostnameVerifier != null)
     {
-      aBP.getRequestContext ().put ("com.sun.xml.ws.transport.https.client.hostname.verifier", m_aHostnameVerifier);
-      aBP.getRequestContext ().put ("com.sun.xml.internal.ws.transport.https.client.hostname.verifier",
-                                    m_aHostnameVerifier);
+      aRequestContext.put ("com.sun.xml.ws.transport.https.client.hostname.verifier", m_aHostnameVerifier);
+      aRequestContext.put ("com.sun.xml.internal.ws.transport.https.client.hostname.verifier", m_aHostnameVerifier);
     }
-
     if (m_nConnectionTimeoutMS >= 0)
     {
-      aBP.getRequestContext ().put ("com.sun.xml.ws.connect.timeout", Integer.valueOf (m_nConnectionTimeoutMS));
-      aBP.getRequestContext ().put ("com.sun.xml.internal.ws.connect.timeout",
-                                    Integer.valueOf (m_nConnectionTimeoutMS));
+      aRequestContext.put ("com.sun.xml.ws.connect.timeout", Integer.valueOf (m_nConnectionTimeoutMS));
+      aRequestContext.put ("com.sun.xml.internal.ws.connect.timeout", Integer.valueOf (m_nConnectionTimeoutMS));
     }
-
     if (m_nRequestTimeoutMS >= 0)
     {
-      aBP.getRequestContext ().put ("com.sun.xml.ws.request.timeout", Integer.valueOf (m_nRequestTimeoutMS));
-      aBP.getRequestContext ().put ("com.sun.xml.internal.ws.request.timeout", Integer.valueOf (m_nRequestTimeoutMS));
+      aRequestContext.put ("com.sun.xml.ws.request.timeout", Integer.valueOf (m_nRequestTimeoutMS));
+      aRequestContext.put ("com.sun.xml.internal.ws.request.timeout", Integer.valueOf (m_nRequestTimeoutMS));
     }
+    if (StringHelper.hasText (m_sUserName))
+    {
+      aRequestContext.put (BindingProvider.USERNAME_PROPERTY, m_sUserName);
+      aRequestContext.put (BindingProvider.PASSWORD_PROPERTY, m_sPassword);
+    }
+    if (hasSOAPAction ())
+    {
+      aRequestContext.put (BindingProvider.SOAPACTION_USE_PROPERTY, Boolean.TRUE);
+      aRequestContext.put (BindingProvider.SOAPACTION_URI_PROPERTY, m_sSOAPAction);
+    }
+    if (CollectionHelper.isNotEmpty (m_aHTTPHeaders))
+    {
+      aRequestContext.put (MessageContext.HTTP_REQUEST_HEADERS, m_aHTTPHeaders);
+    }
+    if (m_eCookiesSupport.isDefined ())
+    {
+      aRequestContext.put (BindingProvider.SESSION_MAINTAIN_PROPERTY, m_eCookiesSupport.getAsBooleanObj ());
+    }
+
+    customizeRequestContext (aRequestContext);
 
     @SuppressWarnings ("rawtypes")
     final List <Handler> aHandlers = aBP.getBinding ().getHandlerChain ();
