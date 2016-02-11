8a865657ed1666f5cbcf388755325f990e84fd4b
Philip Helger <philip@helger.com>
2016-02-11 17:03:24 +0100

    Simplified testing

--- ph-commons/src/test/java/com/helger/commons/codec/IByteArrayCodecTest.java
+++ ph-commons/src/test/java/com/helger/commons/codec/IByteArrayCodecTest.java
@@ -3,8 +3,12 @@ package com.helger.commons.codec;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertNotNull;
 
+import javax.annotation.Nonnull;
+
 import org.junit.Test;
 
+import com.helger.commons.charset.CCharset;
+import com.helger.commons.charset.CharsetManager;
 import com.helger.commons.collection.ArrayHelper;
 import com.helger.commons.random.VerySecureRandom;
 
@@ -13,17 +17,10 @@ import com.helger.commons.random.VerySecureRandom;
  *
  * @author Philip Helger
  */
-public final class ICodecTest
+public final class IByteArrayCodecTest
 {
-
-  private void _testCodec (final IByteArrayCodec c)
+  private void _testCodec (@Nonnull final IByteArrayCodec c, @Nonnull final byte [] aSrcBytes)
   {
-    final int nBytes = 256;
-
-    // Get random bytes
-    final byte [] aSrcBytes = new byte [nBytes];
-    VerySecureRandom.getInstance ().nextBytes (aSrcBytes);
-
     // all
     {
       // Encode
@@ -39,9 +36,11 @@ public final class ICodecTest
     }
 
     // Encode partial
+    final int nBytes = aSrcBytes.length;
+    if (nBytes >= 4)
     {
       // Encode
-      final byte [] aEncoded = c.getEncoded (aSrcBytes, 5, nBytes - 20);
+      final byte [] aEncoded = c.getEncoded (aSrcBytes, 1, nBytes - 2);
       assertNotNull (c.getClass ().getName () + "", aEncoded);
 
       // Decode all (of partial)
@@ -49,7 +48,34 @@ public final class ICodecTest
       assertNotNull (c.getClass ().getName (), aDecoded);
 
       // Compare all with source
-      assertArrayEquals (c.getClass ().getName (), ArrayHelper.getCopy (aSrcBytes, 5, nBytes - 20), aDecoded);
+      assertArrayEquals (c.getClass ().getName (), ArrayHelper.getCopy (aSrcBytes, 1, nBytes - 2), aDecoded);
+    }
+  }
+
+  private void _testCodec (@Nonnull final IByteArrayCodec c)
+  {
+    _testCodec (c, new byte [0]);
+    _testCodec (c, CharsetManager.getAsBytes ("Hallo JÜnit", CCharset.CHARSET_ISO_8859_1_OBJ));
+    _testCodec (c, CharsetManager.getAsBytes ("Hallo JÜnit", CCharset.CHARSET_UTF_8_OBJ));
+
+    // Get random bytes
+    final byte [] aRandomBytes = new byte [256];
+    VerySecureRandom.getInstance ().nextBytes (aRandomBytes);
+    _testCodec (c, aRandomBytes);
+
+    for (int i = 0; i < 256; ++i)
+    {
+      final byte [] aBuf = new byte [i];
+
+      // build ascending identity field
+      for (int j = 0; j < i; ++j)
+        aBuf[j] = (byte) j;
+      _testCodec (c, aBuf);
+
+      // build constant field with all the same byte
+      for (int j = 0; j < i; ++j)
+        aBuf[j] = (byte) i;
+      _testCodec (c, aBuf);
     }
   }
 
