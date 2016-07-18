807703b194178c44218d975d4751e324fe79a385
Philip Helger <philip@helger.com>
2016-07-18 11:24:49 +0200

    Moved classes

--- ph-commons/src/test/java/com/helger/commons/supplementary/test/code/MainDoubleToString.java
+++ ph-commons/src/test/java/com/helger/commons/supplementary/test/code/MainDoubleToString.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.commons.supplementary.test.benchmark;
+package com.helger.commons.supplementary.test.code;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
--- ph-commons/src/test/java/com/helger/commons/supplementary/test/code/StringTrieFuncTest.java
+++ ph-commons/src/test/java/com/helger/commons/supplementary/test/code/StringTrieFuncTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.commons.supplementary.test.benchmark;
+package com.helger.commons.supplementary.test.code;
 
 import java.util.List;
 
