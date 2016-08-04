5bba7a8dfdb2f0c428b930ba063d59d6ef2c358c
phax <philip@helger.com>
2016-08-04 09:54:34 +0200

    Added visitor

---
+++ ph-json/src/main/java/com/helger/json/visit/IJsonVisitorCallback.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.json.visit;
+
+import javax.annotation.Nonnull;
+
+import com.helger.commons.callback.ICallback;
+import com.helger.json.IJsonArray;
+import com.helger.json.IJsonObject;
+import com.helger.json.IJsonValue;
+
+/**
+ * Callback interface for visiting the JSON hierarchy.
+ *
+ * @author Philip Helger
+ */
+public interface IJsonVisitorCallback extends ICallback
+{
+  /**
+   * Invoke for each simple value.
+   *
+   * @param aValue
+   *        The current value. Never <code>null</code>.
+   */
+  default void onJsonValue (@Nonnull final IJsonValue aValue)
+  {}
+
+  /**
+   * Invoked before iterating the children of an array.
+   *
+   * @param aValue
+   *        The current array. Never <code>null</code>.
+   */
+  default void onJsonArrayStart (@Nonnull final IJsonArray aValue)
+  {}
+
+  /**
+   * Invoked after iterating the children of an array.
+   *
+   * @param aValue
+   *        The current array. Never <code>null</code>.
+   */
+  default void onJsonArrayEnd (@Nonnull final IJsonArray aValue)
+  {}
+
+  /**
+   * Invoked before iterating the children of an object.
+   *
+   * @param aValue
+   *        The current object. Never <code>null</code>.
+   */
+  default void onJsonObjectStart (@Nonnull final IJsonObject aValue)
+  {}
+
+  /**
+   * Invoked for each Json object element. The value is handled with the
+   * existing methods like {@link #onJsonValue(IJsonValue)} etc.
+   *
+   * @param sName
+   *        The current object's element name. Never <code>null</code>.
+   */
+  default void onJsonObjectElementName (@Nonnull final String sName)
+  {}
+
+  /**
+   * Invoked after iterating the children of an object.
+   *
+   * @param aValue
+   *        The current object. Never <code>null</code>.
+   */
+  default void onJsonObjectEnd (@Nonnull final IJsonObject aValue)
+  {}
+}
---
+++ ph-json/src/main/java/com/helger/json/visit/JsonVisitor.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.json.visit;
+
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.PresentForCodeCoverage;
+import com.helger.json.IJson;
+import com.helger.json.IJsonArray;
+import com.helger.json.IJsonObject;
+import com.helger.json.IJsonValue;
+
+/**
+ * Recursively visit all Json nodes and invoke the provided callback.
+ *
+ * @author Philip Helger
+ */
+@Immutable
+public final class JsonVisitor
+{
+  @PresentForCodeCoverage
+  private static final JsonVisitor s_aInstance = new JsonVisitor ();
+
+  private JsonVisitor ()
+  {}
+
+  private static void _recursiveVisit (@Nonnull final IJson aJson, @Nonnull final IJsonVisitorCallback aCB)
+  {
+    if (aJson.isValue ())
+    {
+      // Simple value
+      aCB.onJsonValue ((IJsonValue) aJson);
+    }
+    else
+      // Complex (array or object)
+      if (aJson.isArray ())
+      {
+        final IJsonArray aArray = (IJsonArray) aJson;
+        aCB.onJsonArrayStart (aArray);
+
+        for (final IJson aChild : aArray)
+          _recursiveVisit (aChild, aCB);
+
+        aCB.onJsonArrayEnd (aArray);
+      }
+      else
+      {
+        // Must be an object
+        final IJsonObject aObject = (IJsonObject) aJson;
+        aCB.onJsonObjectStart (aObject);
+
+        for (final Map.Entry <String, IJson> aEntry : aObject)
+        {
+          aCB.onJsonObjectElementName (aEntry.getKey ());
+          // Object value
+          _recursiveVisit (aEntry.getValue (), aCB);
+        }
+
+        aCB.onJsonObjectEnd (aObject);
+      }
+  }
+
+  /**
+   * Visit the whole JSON.
+   *
+   * @param aJson
+   *        The JSON to visit. May not be <code>null</code>.
+   * @param aCB
+   *        The callback to be invoked. May not be <code>null</code>.
+   */
+  public static void visit (@Nonnull final IJson aJson, @Nonnull final IJsonVisitorCallback aCB)
+  {
+    ValueEnforcer.notNull (aJson, "JSON");
+    ValueEnforcer.notNull (aCB, "Callback");
+    _recursiveVisit (aJson, aCB);
+  }
+}
