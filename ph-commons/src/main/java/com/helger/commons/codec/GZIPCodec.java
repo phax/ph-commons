8b6b85655d4a68259f064b18f362691e57cffecb
phax <philip@helger.com>
2016-08-21 12:50:00 +0200

    Added GZIPCodec

--- ph-commons/src/main/java/com/helger/commons/codec/GZIPCodec.java
+++ ph-commons/src/main/java/com/helger/commons/codec/GZIPCodec.java
@@ -18,49 +18,27 @@ package com.helger.commons.codec;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterInputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.WillNotClose;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.helger.commons.io.stream.NonBlockingByteArrayInputStream;
 import com.helger.commons.io.stream.NonClosingOutputStream;
 import com.helger.commons.io.stream.StreamHelper;
 
 /**
- * Encoder and decoder for flate compression
+ * Encoder and decoder for GZip compression
  *
  * @author Philip Helger
  */
-public class FlateCodec implements IByteArrayCodec
+public class GZIPCodec implements IByteArrayCodec
 {
-  private static final Logger s_aLogger = LoggerFactory.getLogger (FlateCodec.class);
-
-  public FlateCodec ()
+  public GZIPCodec ()
   {}
-
-  public static boolean isZlibHead (@Nonnull final byte [] buf,
-                                    @Nonnegative final int nOfs,
-                                    @Nonnegative final int nLen)
-  {
-    if (nLen >= 2)
-    {
-      final int b0 = buf[nOfs] & 0xff;
-      final int b1 = buf[nOfs + 1] & 0xff;
-
-      if ((b0 & 0xf) == 8)
-        if ((b0 >> 4) + 8 <= 15)
-          if ((((b0 << 8) + b1) % 31) == 0)
-            return true;
-    }
-    return false;
-  }
 
   public void decode (@Nullable final byte [] aEncodedBuffer,
                       @Nonnegative final int nOfs,
@@ -70,19 +48,16 @@ public class FlateCodec implements IByteArrayCodec
     if (aEncodedBuffer == null || nLen == 0)
       return;
 
-    if (!isZlibHead (aEncodedBuffer, nOfs, nLen))
-      s_aLogger.warn ("ZLib header not found");
-
-    try (final InflaterInputStream aDecodeIS = new InflaterInputStream (new NonBlockingByteArrayInputStream (aEncodedBuffer,
-                                                                                                             nOfs,
-                                                                                                             nLen)))
+    try (final GZIPInputStream aDecodeIS = new GZIPInputStream (new NonBlockingByteArrayInputStream (aEncodedBuffer,
+                                                                                                     nOfs,
+                                                                                                     nLen)))
     {
       if (StreamHelper.copyInputStreamToOutputStream (aDecodeIS, aOS).isFailure ())
-        throw new DecodeException ("Failed to flate decode!");
+        throw new DecodeException ("Failed to GZIP decode!");
     }
     catch (final IOException ex)
     {
-      throw new DecodeException ("Failed to flate encode", ex);
+      throw new DecodeException ("Failed to GZIP encode", ex);
     }
   }
 
@@ -94,16 +69,16 @@ public class FlateCodec implements IByteArrayCodec
     if (aDecodedBuffer == null || nLen == 0)
       return;
 
-    try (final DeflaterOutputStream aEncodeOS = new DeflaterOutputStream (new NonClosingOutputStream (aOS)))
+    try (final GZIPOutputStream aEncodeOS = new GZIPOutputStream (new NonClosingOutputStream (aOS)))
     {
       if (StreamHelper.copyInputStreamToOutputStream (new NonBlockingByteArrayInputStream (aDecodedBuffer, nOfs, nLen),
                                                       aEncodeOS)
                       .isFailure ())
-        throw new EncodeException ("Failed to flate encode!");
+        throw new EncodeException ("Failed to GZIP encode!");
     }
     catch (final IOException ex)
     {
-      throw new EncodeException ("Failed to flate encode", ex);
+      throw new EncodeException ("Failed to GZIP encode", ex);
     }
   }
 }
---
+++ ph-commons/src/test/java/com/helger/commons/codec/GZIPCodecTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.commons.codec;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.nio.charset.Charset;
+
+import org.junit.Test;
+
+import com.helger.commons.charset.CCharset;
+
+/**
+ * Test class for class {@link GZIPCodec}
+ *
+ * @author Philip Helger
+ */
+public final class GZIPCodecTest
+{
+  private static final Charset CHARSET = CCharset.DEFAULT_CHARSET_OBJ;
+
+  @Test
+  public void testDecode ()
+  {
+    final GZIPCodec aCodec = new GZIPCodec ();
+    for (final String s : new String [] { "abc", "", "test blank", "hällö special chars" })
+    {
+      final byte [] aDecoded = s.getBytes (CHARSET);
+      final byte [] aEncoded = aCodec.getEncoded (aDecoded);
+      final byte [] aDecoded2 = aCodec.getDecoded (aEncoded);
+      assertArrayEquals (aDecoded, aDecoded2);
+    }
+  }
+}
