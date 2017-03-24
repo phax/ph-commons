0209702fa51af662de881bd0231d48ff315fdc22
Philip Helger <philip@helger.com>
2017-03-24 14:20:16 +0100

    Replaced forbidden apis

--- ph-matrix/src/main/java/com/helger/matrix/Matrix.java
+++ ph-matrix/src/main/java/com/helger/matrix/Matrix.java
@@ -16,11 +16,11 @@
  */
 package com.helger.matrix;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.io.Serializable;
 import java.io.StreamTokenizer;
 import java.text.NumberFormat;
@@ -36,6 +36,7 @@ import com.helger.commons.annotation.ReturnsMutableCopy;
 import com.helger.commons.collection.ext.CommonsArrayList;
 import com.helger.commons.collection.ext.ICommonsList;
 import com.helger.commons.hashcode.HashCodeGenerator;
+import com.helger.commons.io.stream.StreamHelper;
 import com.helger.commons.lang.ICloneable;
 import com.helger.commons.math.MathHelper;
 import com.helger.commons.string.StringHelper;
@@ -1401,9 +1402,9 @@ public class Matrix implements Serializable, ICloneable <Matrix>
    *         In case of an I/O error
    */
   @Nonnull
-  public static Matrix read (@Nonnull @WillNotClose final BufferedReader aReader) throws IOException
+  public static Matrix read (@Nonnull @WillNotClose final Reader aReader) throws IOException
   {
-    final StreamTokenizer aTokenizer = new StreamTokenizer (aReader);
+    final StreamTokenizer aTokenizer = new StreamTokenizer (StreamHelper.getBuffered (aReader));
 
     // Although StreamTokenizer will parse numbers, it doesn't recognize
     // scientific notation (E or D); however, Double.valueOf does.
