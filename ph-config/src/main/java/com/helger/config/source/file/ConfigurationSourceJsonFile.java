2c6d3b64912c12157750787a6ab63c4a9163657b
Philip Helger <philip@helger.com>
2020-03-15 21:38:47 +0100

    Reading JSON

---
+++ ph-config/src/main/java/com/helger/config/source/file/ConfigurationSourceJsonFile.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (C) 2014-2020 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.config.source.file;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.collection.impl.CommonsLinkedHashMap;
+import com.helger.commons.collection.impl.ICommonsOrderedMap;
+import com.helger.commons.string.ToStringGenerator;
+import com.helger.config.source.IConfigurationSource;
+import com.helger.json.IJson;
+import com.helger.json.IJsonArray;
+import com.helger.json.IJsonObject;
+import com.helger.json.serialize.JsonReader;
+
+/**
+ * Implementation of {@link IConfigurationSource} for properties file based
+ * configuration sources.
+ *
+ * @author Philip Helger
+ */
+public class ConfigurationSourceJsonFile extends AbstractConfigurationSourceFile
+{
+  public static final char LEVEL_SEPARATOR = '.';
+  public static final String ARRAY_SUFFIX_COUNT = "$count";
+
+  private final ICommonsOrderedMap <String, String> m_aProps;
+
+  private static void _recursiveFlattenJson (@Nonnull final String sNamePrefix,
+                                             @Nonnull final IJson aJson,
+                                             @Nonnull final Map <String, String> aTarget)
+  {
+    if (aJson.isValue ())
+      aTarget.put (sNamePrefix, aJson.getAsValue ().getAsString ());
+    else
+      if (aJson.isObject ())
+      {
+        for (final Map.Entry <String, IJson> aEntry : aJson.getAsObject ())
+          _recursiveFlattenJson (sNamePrefix + LEVEL_SEPARATOR + aEntry.getKey (), aEntry.getValue (), aTarget);
+      }
+      else
+      {
+        final IJsonArray aArray = aJson.getAsArray ();
+        aTarget.put (sNamePrefix + LEVEL_SEPARATOR + ARRAY_SUFFIX_COUNT, Integer.toString (aArray.size ()));
+        int nIndex = 0;
+        for (final IJson aEntry : aArray)
+        {
+          _recursiveFlattenJson (sNamePrefix + LEVEL_SEPARATOR + nIndex, aEntry, aTarget);
+          nIndex++;
+        }
+      }
+  }
+
+  /**
+   * Constructor with default priority and default charset
+   *
+   * @param aFile
+   *        File to read from. May not be <code>null</code>.
+   */
+  public ConfigurationSourceJsonFile (@Nonnull final File aFile)
+  {
+    this (SOURCE_TYPE.getDefaultPriority (), aFile, (Charset) null);
+  }
+
+  /**
+   * Constructor with default priority
+   *
+   * @param aFile
+   *        File to read from. May not be <code>null</code>.
+   * @param aCharset
+   *        Character set to use. May be <code>null</code>.
+   */
+  public ConfigurationSourceJsonFile (@Nonnull final File aFile, @Nullable final Charset aCharset)
+  {
+    this (SOURCE_TYPE.getDefaultPriority (), aFile, aCharset);
+  }
+
+  /**
+   * Constructor with default charset
+   *
+   * @param nPriority
+   *        Configuration source priority.
+   * @param aFile
+   *        File to read from. May not be <code>null</code>.
+   */
+  public ConfigurationSourceJsonFile (final int nPriority, @Nonnull final File aFile)
+  {
+    this (nPriority, aFile, (Charset) null);
+  }
+
+  /**
+   * Constructor
+   *
+   * @param nPriority
+   *        Configuration source priority.
+   * @param aFile
+   *        File to read from. May not be <code>null</code>.
+   * @param aCharset
+   *        Character set to use. May be <code>null</code>.
+   */
+  public ConfigurationSourceJsonFile (final int nPriority, @Nonnull final File aFile, @Nullable final Charset aCharset)
+  {
+    super (nPriority, aFile);
+    final JsonReader.Builder aBuilder = JsonReader.builder ()
+                                                  .setSource (aFile,
+                                                              aCharset != null ? aCharset : JsonReader.DEFAULT_CHARSET)
+                                                  .setCustomizeCallback (aParser -> {
+                                                    aParser.setRequireStringQuotes (false);
+                                                    aParser.setAlwaysUseBigNumber (true);
+                                                  });
+    final IJsonObject aProps = aBuilder.hasSource () ? aBuilder.readAsObject () : null;
+    if (aProps != null)
+    {
+      m_aProps = new CommonsLinkedHashMap <> ();
+      for (final Map.Entry <String, IJson> aEntry : aProps)
+        _recursiveFlattenJson (aEntry.getKey (), aEntry.getValue (), m_aProps);
+    }
+    else
+      m_aProps = null;
+  }
+
+  public boolean isInitializedAndUsable ()
+  {
+    return m_aProps != null;
+  }
+
+  @Nullable
+  public String getConfigurationValue (@Nonnull @Nonempty final String sKey)
+  {
+    return m_aProps == null ? null : m_aProps.get (sKey);
+  }
+
+  @Override
+  public String toString ()
+  {
+    return ToStringGenerator.getDerived (super.toString ()).append ("Properties", m_aProps).getToString ();
+  }
+}
---
+++ ph-config/src/test/java/com/helger/config/source/file/ConfigurationSourceJsonFileTest.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (C) 2014-2020 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.config.source.file;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Test;
+
+import com.helger.commons.mock.CommonsTestHelper;
+import com.helger.config.source.EConfigSourceType;
+
+/**
+ * Test class for class {@link ConfigurationSourceJsonFile}.
+ *
+ * @author Philip Helger
+ */
+public final class ConfigurationSourceJsonFileTest
+{
+  private static final File f = new File ("src/test/resources/file/test.json");
+
+  @Test
+  public void testBasic ()
+  {
+    final ConfigurationSourceJsonFile c = new ConfigurationSourceJsonFile (f);
+    assertSame (EConfigSourceType.FILE, c.getSourceType ());
+    assertEquals (EConfigSourceType.FILE.getDefaultPriority (), c.getPriority ());
+    assertTrue (c.isInitializedAndUsable ());
+    assertSame (f, c.getFile ());
+    assertEquals ("string", c.getConfigurationValue ("element1"));
+    assertEquals ("2", c.getConfigurationValue ("element2"));
+    assertEquals ("1234", c.getConfigurationValue ("element5.network.port"));
+    assertNull (c.getConfigurationValue ("what a mess"));
+
+    // Check array
+    assertEquals ("4", c.getConfigurationValue ("element6.$count"));
+    assertEquals ("17", c.getConfigurationValue ("element6.0"));
+    assertEquals ("12", c.getConfigurationValue ("element6.3"));
+
+    assertEquals ("3", c.getConfigurationValue ("element7.$count"));
+    assertEquals ("10", c.getConfigurationValue ("element7.0.key"));
+    assertNull (c.getConfigurationValue ("element7.0.value"));
+    assertEquals ("3", c.getConfigurationValue ("element7.0.value.$count"));
+    assertEquals ("bar", c.getConfigurationValue ("element7.0.value.2"));
+    assertEquals ("plain value", c.getConfigurationValue ("element7.1"));
+    assertEquals ("blub", c.getConfigurationValue ("element7.2.value.0"));
+
+    CommonsTestHelper.testDefaultImplementationWithEqualContentObject (c, new ConfigurationSourceJsonFile (f));
+    CommonsTestHelper.testDefaultImplementationWithDifferentContentObject (c,
+                                                                           new ConfigurationSourceJsonFile (1234, f));
+    CommonsTestHelper.testDefaultImplementationWithDifferentContentObject (c,
+                                                                           new ConfigurationSourceJsonFile (new File ("bla")));
+  }
+
+  @Test
+  public void testExplicitCharset ()
+  {
+    final ConfigurationSourceJsonFile c = new ConfigurationSourceJsonFile (f, StandardCharsets.ISO_8859_1);
+    assertSame (EConfigSourceType.FILE, c.getSourceType ());
+    assertEquals (EConfigSourceType.FILE.getDefaultPriority (), c.getPriority ());
+    assertTrue (c.isInitializedAndUsable ());
+    assertEquals ("string", c.getConfigurationValue ("element1"));
+    assertEquals ("2", c.getConfigurationValue ("element2"));
+    assertEquals ("1234", c.getConfigurationValue ("element5.network.port"));
+  }
+
+  @Test
+  public void testDifferentPriority ()
+  {
+    final ConfigurationSourceJsonFile c = new ConfigurationSourceJsonFile (2323, f);
+    assertSame (EConfigSourceType.FILE, c.getSourceType ());
+    assertEquals (2323, c.getPriority ());
+    assertTrue (c.isInitializedAndUsable ());
+    assertEquals ("string", c.getConfigurationValue ("element1"));
+    assertEquals ("2", c.getConfigurationValue ("element2"));
+    assertEquals ("1234", c.getConfigurationValue ("element5.network.port"));
+  }
+
+  @Test
+  public void testDifferentPriorityAndExplicitCharset ()
+  {
+    final ConfigurationSourceJsonFile c = new ConfigurationSourceJsonFile (2323, f, StandardCharsets.ISO_8859_1);
+    assertSame (EConfigSourceType.FILE, c.getSourceType ());
+    assertEquals (2323, c.getPriority ());
+    assertTrue (c.isInitializedAndUsable ());
+    assertEquals ("string", c.getConfigurationValue ("element1"));
+    assertEquals ("2", c.getConfigurationValue ("element2"));
+    assertEquals ("1234", c.getConfigurationValue ("element5.network.port"));
+  }
+
+  @Test
+  public void testNonExisting ()
+  {
+    final File f2 = new File ("bla");
+    final ConfigurationSourceJsonFile c = new ConfigurationSourceJsonFile (f2);
+    assertSame (EConfigSourceType.FILE, c.getSourceType ());
+    assertEquals (EConfigSourceType.FILE.getDefaultPriority (), c.getPriority ());
+    assertFalse (c.isInitializedAndUsable ());
+    assertSame (f2, c.getFile ());
+    assertNull (c.getConfigurationValue ("element1"));
+    assertNull (c.getConfigurationValue ("element2"));
+    assertNull (c.getConfigurationValue ("element5.network.port"));
+  }
+}
