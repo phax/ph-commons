a69505360bd7b2aa293a4aa9b38af1ab2e549f9c
Philip Helger <philip@helger.com>
2021-01-01 13:46:00 +0100

    Updated to 2021

--- ph-jaxb/src/main/java/com/helger/jaxb/adapter/AdapterLocalDateTime.java
+++ ph-jaxb/src/main/java/com/helger/jaxb/adapter/AdapterLocalDateTime.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014-2020 Philip Helger (www.helger.com)
+ * Copyright (C) 2014-2021 Philip Helger (www.helger.com)
  * philip[at]helger[dot]com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.helger.jaxb.adapter;
 
 import java.time.LocalDateTime;
