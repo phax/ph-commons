78e349de5d308bd6d99808c8e0379e99c7a369f8
Philip Helger <philip@helger.com>
2024-09-10 14:43:17 +0200

    Improved docs

--- ph-config/src/main/java/com/helger/config/source/envvar/EnvVarHelper.java
+++ ph-config/src/main/java/com/helger/config/source/envvar/EnvVarHelper.java
@@ -17,7 +17,6 @@
 package com.helger.config.source.envvar;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 
 import com.helger.commons.ValueEnforcer;
@@ -33,21 +32,53 @@ public final class EnvVarHelper
 {
   public static final char DEFAULT_REPLACEMENT_CHAR = '_';
   // REGEX_ENV_VAR = "[a-zA-Z_]+[a-zA-Z0-9_]*";
+  public static final char REPLACEMENT_CHAR_TO_SKIP = 0;
 
   private EnvVarHelper ()
   {}
 
+  /**
+   * Check if the provided character is a valid first character for an
+   * environment variable.
+   *
+   * @param c
+   *        The character to check
+   * @return <code>true</code> if it is valid, <code>false</code> if not
+   */
   public static boolean isValidFirstChar (final char c)
   {
     return (c >= 'A' && c <= 'Z') || c == '_';
   }
 
+  /**
+   * Check if the provided character is a valid second or following character
+   * for an environment variable.
+   *
+   * @param c
+   *        The character to check
+   * @return <code>true</code> if it is valid, <code>false</code> if not
+   */
   public static boolean isValidFollowingChar (final char c)
   {
     return isValidFirstChar (c) || (c >= '0' && c <= '9');
   }
 
-  @Nullable
+  /**
+   * Get a unified system environment variable name from the provided property
+   * name. This means uppercasing all characters and replacing invalid chars
+   * with a replacement character.
+   *
+   * @param sName
+   *        The property name to convert. May neither be <code>null</code> nor
+   *        empty.
+   * @param cReplacement
+   *        The character that should be used to replace invalid characters. See
+   *        {@link #DEFAULT_REPLACEMENT_CHAR}. If character `\u0000´ is used,
+   *        the invalid character is simply ignored. See
+   *        {@link #REPLACEMENT_CHAR_TO_SKIP}.
+   * @return The
+   */
+  @Nonnull
   public static String getUnifiedSysEnvName (@Nonnull @Nonempty final String sName, final char cReplacement)
   {
     ValueEnforcer.notEmpty (sName, "Name");
@@ -59,7 +90,11 @@ public final class EnvVarHelper
       // Manually upper case for quicker tests afterwards
       final char cUpper = Character.toUpperCase (c);
       final boolean bIsValid = nIndex == 0 ? isValidFirstChar (cUpper) : isValidFollowingChar (cUpper);
-      ret.append (bIsValid ? cUpper : cReplacement);
+      if (bIsValid)
+        ret.append (cUpper);
+      else
+        if (cReplacement != REPLACEMENT_CHAR_TO_SKIP)
+          ret.append (cReplacement);
       nIndex++;
     }
     return ret.toString ();
