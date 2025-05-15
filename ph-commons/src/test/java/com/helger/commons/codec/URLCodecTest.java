38afb1743a727094ef07d20a6db0d42b7c547dc2
Philip Helger <philip@helger.com>
2025-05-15 20:29:10 +0200

    Added test

---
+++ ph-commons/src/test/java/com/helger/commons/codec/URLCodecTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014-2025 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.commons.codec;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Test;
+
+/**
+ * Test class for class {@link URLCodec}.
+ *
+ * @author Philip Helger
+ */
+public final class URLCodecTest
+{
+  @Test
+  public void testDecode ()
+  {
+    final String sEncoded = "https://staging-smp.nymus.eu/iso6523-actorid-upis%3A%3A9925%3Abe0758688864/services/busdox-docid-qns%3A%3Aurn%3Aoasis%3Anames%3Aspecification%3Aubl%3Aschema%3Axsd%3ACreditNote-2%3A%3ACreditNote%23%23urn%3Acen.eu%3Aen16931%3A2017%23compliant%23urn%3Afdc%3Apeppol.eu%3A2017%3Apoacc%3Abilling%3A3.0%3A%3A2.1";
+    final String sDecoded = new URLCodec ().getDecodedAsString (sEncoded, StandardCharsets.ISO_8859_1);
+    assertEquals ("https://staging-smp.nymus.eu/iso6523-actorid-upis::9925:be0758688864/services/busdox-docid-qns::urn:oasis:names:specification:ubl:schema:xsd:CreditNote-2::CreditNote##urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0::2.1",
+                  sDecoded);
+  }
+}
