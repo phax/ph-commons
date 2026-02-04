119baa879014467eecd96a9d7374fda5f2d98e70
Philip Helger <philip@helger.com>
2026-02-04 12:08:19 +0100

    Added possibility to change the reserve count

--- ph-base/src/main/java/com/helger/base/id/factory/AbstractPersistingLongIDFactory.java
+++ ph-base/src/main/java/com/helger/base/id/factory/AbstractPersistingLongIDFactory.java
@@ -20,6 +20,7 @@ import com.helger.annotation.Nonnegative;
 import com.helger.annotation.concurrent.ELockType;
 import com.helger.annotation.concurrent.GuardedBy;
 import com.helger.annotation.concurrent.IsLocked;
+import com.helger.annotation.concurrent.MustBeLocked;
 import com.helger.annotation.concurrent.ThreadSafe;
 import com.helger.base.concurrent.SimpleLock;
 import com.helger.base.enforce.ValueEnforcer;
@@ -27,10 +28,9 @@ import com.helger.base.hashcode.HashCodeGenerator;
 import com.helger.base.tostring.ToStringGenerator;
 
 /**
- * This implementation of {@link ILongIDFactory} reads IDs from a device. It
- * does it by reserving a range of <em>n</em> IDs so that not each ID
- * reservation requires IO. If only 1 ID is effectively used, the other
- * <em>n</em>-1 IDs are lost and will never be assigned to any object again.
+ * This implementation of {@link ILongIDFactory} reads IDs from a device. It does it by reserving a
+ * range of <em>n</em> IDs so that not each ID reservation requires IO. If only 1 ID is effectively
+ * used, the other <em>n</em>-1 IDs are lost and will never be assigned to any object again.
  *
  * @author Philip Helger
  */
@@ -38,7 +38,8 @@ import com.helger.base.tostring.ToStringGenerator;
 public abstract class AbstractPersistingLongIDFactory implements ILongIDFactory
 {
   protected final SimpleLock m_aLock = new SimpleLock ();
-  private final int m_nReserveCount;
+  @GuardedBy ("m_aLock")
+  private int m_nReserveCount;
   @GuardedBy ("m_aLock")
   private long m_nID = 0L;
   @GuardedBy ("m_aLock")
@@ -48,32 +49,52 @@ public abstract class AbstractPersistingLongIDFactory implements ILongIDFactory
    * Constructor.
    *
    * @param nReserveCount
-   *        The number of IDs to reserve per persistence layer access. Must be
-   *        &gt; 0.
+   *        The number of IDs to reserve per persistence layer access. Must be &gt; 0.
    */
   protected AbstractPersistingLongIDFactory (@Nonnegative final int nReserveCount)
   {
-    ValueEnforcer.isGT0 (nReserveCount, "ReserveCount");
-    m_nReserveCount = nReserveCount;
+    setReserveCount (nReserveCount);
   }
 
   /**
-   * @return The number of IDs to reserve, as provided in the constructor.
-   *         Always &gt; 0.
+   * @return The number of IDs to reserve, as provided in the constructor. Always &gt; 0.
    */
   @Nonnegative
   public final int getReserveCount ()
   {
-    // As reserve count is final, we don't need to lock access to it!
+    return m_aLock.lockedInt ( () -> m_nReserveCount);
+  }
+
+  /**
+   * @return The number of IDs to reserve, as provided in the constructor. Always &gt; 0.
+   * @since 12.1.3
+   */
+  @Nonnegative
+  @MustBeLocked (ELockType.WRITE)
+  protected final int internalGetReserveCountUnlocked ()
+  {
     return m_nReserveCount;
   }
 
   /**
-   * Read the current ID from the device. In case the method is called for a
-   * non-initialized device, 0 should be returned.<br>
-   * The update should write the read value plus the passed reserve count back
-   * to the device. This method should perform an atomic read and update to
-   * avoid that ID can be reused.<br>
+   * Dynamically set the reserve count of this factory. May be a performance improvement when this
+   * is used in an area where many IDs are required.
+   *
+   * @param nReserveCount
+   *        The reserve count to use. Must be &gt; 0.
+   * @since 12.1.3
+   */
+  public final void setReserveCount (@Nonnegative final int nReserveCount)
+  {
+    ValueEnforcer.isGT0 (nReserveCount, "ReserveCount");
+    m_aLock.locked ( () -> m_nReserveCount = nReserveCount);
+  }
+
+  /**
+   * Read the current ID from the device. In case the method is called for a non-initialized device,
+   * 0 should be returned.<br>
+   * The update should write the read value plus the passed reserve count back to the device. This
+   * method should perform an atomic read and update to avoid that ID can be reused.<br>
    * Pseudo code:
    *
    * <pre>
@@ -87,18 +108,12 @@ public abstract class AbstractPersistingLongIDFactory implements ILongIDFactory
    *
    * @param nReserveCount
    *        the number that should be added to the read value. Always &gt; 0.
-   * @return 0 if this method is called for a non-initialized device, the value
-   *         read from the device otherwise or
-   *         {@link com.helger.base.CGlobal#ILLEGAL_ULONG} in case of an
-   *         error.
+   * @return 0 if this method is called for a non-initialized device, the value read from the device
+   *         otherwise or {@link com.helger.base.CGlobal#ILLEGAL_ULONG} in case of an error.
    */
   @IsLocked (ELockType.WRITE)
   protected abstract long readAndUpdateIDCounter (@Nonnegative int nReserveCount);
 
-  /*
-   * Note: this implementation must be synchronized because the method calling
-   * this only uses a readLock!
-   */
   public final long getNewID ()
   {
     m_aLock.lock ();
