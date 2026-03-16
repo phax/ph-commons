f1e68ba25c48bbc7d444f2b6d3b96b95e97f632e
Philip Helger <philip@helger.com>
2026-03-16 22:22:54 +0100

    Added comments

--- ph-http/src/main/java/com/helger/http/CHttpHeader.java
+++ ph-http/src/main/java/com/helger/http/CHttpHeader.java
@@ -27,56 +27,86 @@ import com.helger.annotation.style.PresentForCodeCoverage;
 @Immutable
 public final class CHttpHeader
 {
+  /** RFC 9110 Section 12.5.1 */
   public static final String ACCEPT = "Accept";
+  /** RFC 9110 Section 12.5.2 */
   public static final String ACCEPT_CHARSET = "Accept-Charset";
+  /** RFC 9110 Section 12.5.3 */
   public static final String ACCEPT_ENCODING = "Accept-Encoding";
+  /** RFC 9110 Section 12.5.4 */
   public static final String ACCEPT_LANGUAGE = "Accept-Language";
+  /** RFC 5789 Section 3.1 */
   public static final String ACCEPT_PATCH = "Accept-Patch";
+  /** RFC 9111 Section 5.1 */
   public static final String AGE = "Age";
+  /** RFC 9110 Section 10.2.1 */
   public static final String ALLOW = "Allow";
+  /** RFC 9111 Section 5.2 */
   public static final String CACHE_CONTROL = "Cache-Control";
+  /** RFC 9110 Section 7.6.1 */
   public static final String CONNECTION = "Connection";
+  /** RFC 2045 Section 8 */
   public static final String CONTENT_DESCRIPTION = "Content-Description";
+  /** RFC 6266 */
   public static final String CONTENT_DISPOSITION = "Content-Disposition";
+  /** RFC 9110 Section 8.4 */
   public static final String CONTENT_ENCODING = "Content-Encoding";
+  /** RFC 2045 Section 7 */
   public static final String CONTENT_ID = "Content-ID";
+  /** RFC 9110 Section 8.3 */
   public static final String CONTENT_TYPE = "Content-Type";
+  /** RFC 6265 Section 5.4 */
   public static final String COOKIE = "Cookie";
+  /** RFC 9110 Section 6.6.1 */
   public static final String DATE = "Date";
+  /** RFC 9110 Section 8.8.3 */
   public static final String ETAG = "ETag";
+  /** RFC 9111 Section 5.3 */
   public static final String EXPIRES = "Expires";
+  /** RFC 7239 */
   public static final String FORWARDED = "Forwarded";
+  /** RFC 9110 Section 7.2 */
   public static final String HOST = "Host";
+  /** RFC 9110 Section 13.1.2 */
   public static final String IF_NON_MATCH = "If-None-Match";
+  /** RFC 9110 Section 13.1.1 */
   public static final String IF_MATCH = "If-Match";
+  /** RFC 9110 Section 13.1.3 */
   public static final String IF_MODIFIED_SINCE = "If-Modified-Since";
+  /** RFC 9110 Section 13.1.4 */
   public static final String IF_UNMODIFIED_SINCE = "If-Unmodified-Since";
+  /** RFC 9110 Section 8.8.2 */
   public static final String LAST_MODIFIED = "Last-Modified";
+  /** RFC 9110 Section 10.2.2 */
   public static final String LOCATION = "Location";
   /**
    * Deprecated in RFC 9111 Section 5.4. Use {@link #CACHE_CONTROL} instead.
    */
   @Deprecated (forRemoval = false, since = "12.1.5")
   public static final String PRAGMA = "Pragma";
+  /** RFC 9110 Section 10.1.3 */
   public static final String REFERER = "Referer";
+  /** RFC 9110 Section 10.1.5 */
   public static final String USER_AGENT = "User-Agent";
+  /** RFC 9110 Section 12.5.5 */
   public static final String VARY = "Vary";
   /**
    * Removed in RFC 9111 (HTTP Caching, 2022). No longer defined in the HTTP standard.
    */
   @Deprecated (forRemoval = false, since = "12.1.5")
   public static final String WARNING = "Warning";
+  /** RFC 9110 Section 11.6.2 */
   public static final String AUTHORIZATION = "Authorization";
+  /** RFC 9110 Section 11.7.2 */
   public static final String PROXY_AUTHORIZATION = "Proxy-Authorization";
+  /** RFC 9110 Section 11.6.1 */
   public static final String WWW_AUTHENTICATE = "WWW-Authenticate";
   /**
-   * https://www.owasp.org/index.php/HTTP_Strict_Transport_Security<br>
-   * e.g. "max-age=16070400; includeSubDomains"
+   * RFC 6797. E.g. "max-age=16070400; includeSubDomains"
    */
   public static final String STRICT_TRANSPORT_SECURITY = "Strict-Transport-Security";
   /**
-   * https://www.owasp.org/index.php/List_of_useful_HTTP_headers<br>
-   * e.g. X-Frame-Options: deny
+   * RFC 7034. E.g. X-Frame-Options: deny
    */
   public static final String X_FRAME_OPTIONS = "X-Frame-Options";
   /**
@@ -102,11 +132,12 @@ public final class CHttpHeader
   public static final String PROXY = "proxy";
 
   /**
-   * The Retry-After response-header field can be used with a 503 (Service Unavailable) response to
-   * indicate how long the service is expected to be unavailable to the requesting client. This
-   * field MAY also be used with any 3xx (Redirection) response to indicate the minimum time the
-   * user-agent is asked wait before issuing the redirected request. The value of this field can be
-   * either an HTTP-date or an integer number of seconds (in decimal) after the time of the response
+   * RFC 9110 Section 10.2.3. The Retry-After response-header field can be used with a 503 (Service
+   * Unavailable) response to indicate how long the service is expected to be unavailable to the
+   * requesting client. This field MAY also be used with any 3xx (Redirection) response to indicate
+   * the minimum time the user-agent is asked wait before issuing the redirected request. The value
+   * of this field can be either an HTTP-date or an integer number of seconds (in decimal) after the
+   * time of the response
    */
   public static final String RETRY_AFTER = "Retry-After";
 
@@ -127,7 +158,7 @@ public final class CHttpHeader
   @Deprecated (forRemoval = false, since = "12.1.5")
   public static final String X_DEVICE_USER_AGENT = "x-device-user-agent";
 
-  // HTML
+  /** RFC 9110 Section 8.5 */
   public static final String CONTENT_LANGUAGE = "Content-Language";
   /**
    * HTML 4.01 only; obsolete in HTML5.
@@ -162,9 +193,11 @@ public final class CHttpHeader
   @Deprecated (forRemoval = false, since = "12.1.5")
   public static final String X_UA_COMPATIBLE = "X-UA-Compatible";
 
-  // Response codes
+  /** RFC 9110 Section 8.6 */
   public static final String CONTENT_LENGTH = "Content-Length";
+  /** RFC 6265 Section 4.1 */
   public static final String SET_COOKIE = "Set-Cookie";
+  /** RFC 9112 Section 6.1 */
   public static final String TRANSFER_ENCODING = "Transfer-Encoding";
 
   // CORS
@@ -198,23 +231,34 @@ public final class CHttpHeader
   public static final String PROFILE_DIFF = "Profile-Diff";
   public static final String WAP_PROFILE_DIFF = "Wap-Profile-Diff";
 
-  // AS2 headers
+  // AS2 headers (RFC 4130)
+  /** RFC 4130 Section 6.1 */
   public static final String AS2_FROM = "AS2-From";
+  /** RFC 4130 Section 6.1 */
   public static final String AS2_TO = "AS2-To";
+  /** RFC 4130 Section 6.1 */
   public static final String AS2_VERSION = "AS2-Version";
+  /** RFC 2045 Section 6 */
   public static final String CONTENT_TRANSFER_ENCODING = "Content-Transfer-Encoding";
+  /** RFC 8098 Section 2.2 */
   public static final String DISPOSITION_NOTIFICATION_OPTIONS = "Disposition-Notification-Options";
-  /** The URL where the async MDN should be send to - limited to RFC 2822 */
+  /** RFC 8098 Section 2.1. The URL where the async MDN should be sent to - limited to RFC 2822 */
   public static final String DISPOSITION_NOTIFICATION_TO = "Disposition-Notification-To";
+  /** RFC 9110 Section 10.1.2 */
   public static final String FROM = "From";
+  /** RFC 5322 Section 3.6.4 */
   public static final String MESSAGE_ID = "Message-ID";
+  /** RFC 2045 Section 4 */
   public static final String MIME_VERSION = "Mime-Version";
-  /** The URL where the async MDN should be send to */
+  /** RFC 4130 Section 7.3. The URL where the async MDN should be sent to */
   public static final String RECEIPT_DELIVERY_OPTION = "Receipt-Delivery-Option";
+  /** RFC 4130 Section 6.2 */
   public static final String RECIPIENT_ADDRESS = "Recipient-Address";
+  /** RFC 9110 Section 10.2.4 */
   public static final String SERVER = "Server";
+  /** RFC 5322 Section 3.6.5 */
   public static final String SUBJECT = "Subject";
-  /** Defined by RFC 6017 */
+  /** RFC 6017 */
   public static final String EDIINT_FEATURES = "EDIINT-Features";
 
   // Standard headers from RFC 9110 (HTTP Semantics)
