2975afd30c678c9d9ddc9b1f243a25d2534d866b
Philip Helger <philip@helger.com>
2026-04-28 22:01:15 +0200

    Added additional MD algorithms

--- ph-security/src/main/java/com/helger/security/messagedigest/EMessageDigestAlgorithm.java
+++ ph-security/src/main/java/com/helger/security/messagedigest/EMessageDigestAlgorithm.java
@@ -39,7 +39,15 @@ public enum EMessageDigestAlgorithm
   SHA_224 ("SHA-224"),
   SHA_256 ("SHA-256"),
   SHA_384 ("SHA-384"),
-  SHA_512 ("SHA-512");
+  SHA_512 ("SHA-512"),
+  // SHA-512 truncated
+  SHA_512_224 ("SHA-512/224"),
+  // SHA-512 truncated
+  SHA_512_256 ("SHA-512/256"),
+  SHA3_224 ("SHA3-224"),
+  SHA3_256 ("SHA3-256"),
+  SHA3_384 ("SHA3-384"),
+  SHA3_512 ("SHA3-512");
 
   private final String m_sAlgorithm;
 
